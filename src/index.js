20cf427103e4baa9468cc1732e2482a399e5aae9
smdmori <mortezasamadi1371@gmail.com>
2021-11-01 23:25:49 +0330

    started with react wbsite

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* ToDo */ }
+            </button>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+    render() {
+        const status = 'Next Player: X';
+
+        return (
+            
+        );
+    }
+}
